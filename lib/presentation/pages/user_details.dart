0cd25b90d181a5db5ecd8f1602eae8aed0256027
Huzaifa Ahmed <ahmedhuzaifa13@gmail.com>
2020-12-06 23:23:56 +0500

    design user details page

--- lib/presentation/pages/user_details.dart
+++ lib/presentation/pages/user_details.dart
@@ -9,21 +9,41 @@ class UserDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     final userInfo = context.watch<UserNotifier>().user;
     return Scaffold(
-      // backgroundColor: Colors.black,
+      backgroundColor: Colors.black,
       body: Column(
         children: [
           const SizedBox(height: 50),
           ListTile(
+            isThreeLine: true,
             leading: CircleAvatar(
               backgroundImage: NetworkImage(userInfo.avatar_url),
             ),
             title: Text(
               userInfo.name ?? '',
-              style: TextStyle(color: Colors.black),
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
             ),
             subtitle: Text(
               userInfo.bio ?? '',
-              style: TextStyle(color: Colors.black),
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  '${userInfo.followers} followers',
+                  style: TextStyle(color: Colors.white),
+                ),
+                const SizedBox(width: 20),
+                Text(
+                  '${userInfo.following} following',
+                  style: TextStyle(color: Colors.white),
+                )
+              ],
             ),
           )
         ],
